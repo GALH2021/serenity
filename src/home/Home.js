7c292c56db99ecf0b32308ffb748c0398275aa8d
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-06-15 10:39:46 -0400

    switched to npm from yarn

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,28 @@
+import './Home.css'
+
 export default function Home() {
     return (
-        <h1>hello</h1>
+        <div>
+            <h1>Welcome</h1>
+            <div>
+                <h3>Articles</h3>
+                <img className="home-images" src="https://images.pexels.com/photos/1438190/pexels-photo-1438190.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+                <p className="home-description">sldgknd ejwhf lkehr lknflasnfk  laksfh  lkehfsfaff aif e fi fi dfasdfkhoak fda fsahdfsa ofgs.</p>
+                <button>Atricles</button>
+            </div>
+            <div>
+                <h3>Podcasts</h3>
+                <img className="home-images" src="https://images.pexels.com/photos/5939401/pexels-photo-5939401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+                <p className="home-description">sldgknd ejwhf lkehr lknflasnfk  laksfh  lkehfsfaff aif e fi fi dfasdfkhoak fda fsahdfsa ofgs.</p>
+                <button>Podcasts</button>
+            </div>
+            <div>
+                <h3>Random Thing</h3>
+                <img className="home-images" src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"></img>
+                <p className="home-description">sldgknd ejwhf lkehr lknflasnfk  laksfh  lkehfsfaff aif e fi fi dfasdfkhoak fda fsahdfsa ofgs.</p>
+                <button>Things</button>
+            </div>
+        </div>
+
     )
 }
