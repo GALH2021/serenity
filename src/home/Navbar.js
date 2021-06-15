1c3adb6d336fb40d9478df86a1e7ce77003a475a
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-06-15 00:06:47 -0400

    navbar styled

--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -1,30 +1,40 @@
 import React, { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import Home from './Home';
+import './Navbar.css';
+import { AiFillHome } from "react-icons/ai";
+import { BsFillChatDotsFill, BsFillPersonFill } from "react-icons/bs";
+import { RiBookletLine } from "react-icons/ri";
 
 export default function Navbar() {
     return (
-        <div className="App">
+        <div>
             <Router>
-                <div>
-                    <nav>
-                        <div>
-                            <Link to="/home">Home</Link>
-                            <Link to="/chat">Chat</Link>
-                            <Link to="/questionnaire">Questionnaire</Link>
-                            <Link to="/settings">Settings</Link>
+                <nav>
+                    <div className="navbar-container">
+                        <div className="icon-circle">
+                            <Link to="/home" ><AiFillHome className="navbar-icons" /></Link>
                         </div>
-                        <main>
-                            <Switch>
-                                <div>
-                                    <Route path='/home' component={Home}></Route>
-                                    <Route path='/chat'></Route>
-                                    <Route path='/questionnaire'></Route>
-                                    <Route path='/settings'></Route>
-                                </div>
-                            </Switch>
-                        </main>
-                    </nav>
-                </div>
+                        <div className="icon-circle">
+                            <Link to="/chat"><BsFillChatDotsFill className="navbar-icons" /></Link>
+                        </div>
+                        <div className="icon-circle">
+                            <Link to="/questionnaire"><RiBookletLine className="navbar-icons" /></Link>
+                        </div>
+                        <div className="icon-circle">
+                            <Link to="/settings"><BsFillPersonFill className="navbar-icons" /></Link>
+                        </div>
+                    </div>
+                    <main>
+                        <Switch>
+                            <div>
+                                <Route path='/home' component={Home}></Route>
+                                <Route path='/chat'></Route>
+                                <Route path='/questionnaire'></Route>
+                                <Route path='/settings'></Route>
+                            </div>
+                        </Switch>
+                    </main>
+                </nav>
             </Router>
         </div>
     )
