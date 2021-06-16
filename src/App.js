09b578920b6fcd64ccd44175404fd718b92fad30
aria414 <ayng13@yahoo.com>
2021-06-16 11:08:13 -0400

    main stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
-import Home from './home/Home';
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Home from "./home/Home";
 import datafile from "./resource-pages/datafile";
-import Chat from './chat/Chat';
+import Chat from "./chat/Chat";
 import Resources from "./resource-pages/Resources";
 import SingleResource from "./resource-pages/SingleResource";
 import { AuthProvider } from "./firebase-backend/context/AuthContext";
 import FilterForm from "./questionnaire/filterForm";
-import Footer from './home/Footer';
+import Footer from "./home/Footer";
 import Signup from "./firebase-backend/components/Signup";
 import Login from "./firebase-backend/components/Login";
 import UserProfile from "./firebase-backend/components/UserProfile";
@@ -17,51 +17,60 @@ import { Container } from "react-bootstrap";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillChatDotsFill, BsFillPersonFill } from "react-icons/bs";
 import { RiBookletLine } from "react-icons/ri";
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-
       {/* Bips Part */}
-      <Container
-        className="d-flex align-tiem-center justify-content-center"
-      >
+      <Container className="d-flex align-tiem-center justify-content-center">
         <div className="w-100" style={{ maxWidth: "500px" }}>
           <Router>
             <AuthProvider>
               <Switch>
-                <PrivateRoute exact path="/userProfile" component={UserProfile} />
+                <PrivateRoute
+                  exact
+                  path="/userProfile"
+                  component={UserProfile}
+                />
                 <Route exact path="/signup" component={Signup} />
                 <Route exact path="/login" component={Login} />
-                <Route exact path="/forgotPassword" component={ForgotPassword} />
+                <Route
+                  exact
+                  path="/forgotPassword"
+                  component={ForgotPassword}
+                />
               </Switch>
             </AuthProvider>
           </Router>
         </div>
       </Container>
 
-
-      <FilterForm />
-
+      {/*<FilterForm />*/}
 
       {/* Drew Wilson Part */}
       <Router>
         <nav>
           <div className="navbar-container">
-            <Link to="/home" >
-              <div className="icon-circle"><AiFillHome className="navbar-icons" /></div>
+            <Link to="/home">
+              <div className="icon-circle">
+                <AiFillHome className="navbar-icons" />
+              </div>
             </Link>
             <Link to="/chat">
-              <div className="icon-circle"><BsFillChatDotsFill className="navbar-icons" /></div>
+              <div className="icon-circle">
+                <BsFillChatDotsFill className="navbar-icons" />
+              </div>
             </Link>
             <Link to="/questionnaire">
-              <div className="icon-circle"><RiBookletLine className="navbar-icons" /></div>
+              <div className="icon-circle">
+                <RiBookletLine className="navbar-icons" />
+              </div>
             </Link>
             <Link to="/settings">
-              <div className="icon-circle"><BsFillPersonFill className="navbar-icons" /></div>
+              <div className="icon-circle">
+                <BsFillPersonFill className="navbar-icons" />
+              </div>
             </Link>
           </div>
         </nav>
@@ -72,21 +81,22 @@ function App() {
               <Route exact path="/">
                 <Resources />
               </Route>
-              <Route path='/home' component={Home}></Route>
-              <Route path='/chat' component={Chat}></Route>
-              <Route path='/questionnaire'></Route>
-              <Route path='/settings'></Route>
-              <Route path="/resource/:id" render={(routerProps) => (
-                <SingleResource {...routerProps} resourceData={datafile} />
-              )}
+              <Route path="/home" component={Home}></Route>
+              <Route path="/chat" component={Chat}></Route>
+              <Route path="/questionnaire"></Route>
+              <Route path="/settings"></Route>
+              <Route
+                path="/resource/:id"
+                render={(routerProps) => (
+                  <SingleResource {...routerProps} resourceData={datafile} />
+                )}
               />
             </div>
           </Switch>
           <Footer />
         </main>
       </Router>
-
     </div>
-  )
+  );
 }
 export default App;
